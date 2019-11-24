248b148fc67ce86598bbe638bb6c39924dd6c449
sblau13 <57164486+sblau13@users.noreply.github.com>
2019-11-24 12:47:06 -0600

    Update debugLogicalOperatorDrills.py

--- debugLogicalOperatorDrills.py
+++ debugLogicalOperatorDrills.py
@@ -23,27 +23,33 @@ START HERE
 # )Broken:
 # )4 == 4 AND 3 == 3
 # )Correct:
+4 == 4 and 3 == 3
 
 #2)
 # )Broken:
 # )3 == 3 OR 3 == 4
 # )Correct:
+3 == 3 or 3 == 4
 
 #3)
 # )Broken:
 # )5 == 5 AND 4 == 5
 # )Correct:
+5 == 5 and 4 == 5
 
 #4)
 # )Broken:
 # )4 == 4 OR 4 == 5
 # )Correct:
+4 == 4 or 4 == 5
 
 #5)
 # )Broken:
 # )a = 4
 # )NOT a == 3
 # )Correct:
+a = 4
+"not"a == 3
 
 #6)
 # )Broken:
@@ -51,3 +57,6 @@ START HERE
 # )b = 3
 # )NOT a == b
 # )Correct:
+a = 4
+b = 3
+"not"a == b
